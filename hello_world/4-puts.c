ed7dd02dde7b6b4e4afa822b68985c245fdaba5e
MBK83 <rayanembarek07@gmail.com>
2022-09-30 01:45:49 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -4,9 +4,9 @@
  *   * main - Prints a text
  *     *
  *       * Return: Always 0 (Success)
- *         */
+ **/
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
