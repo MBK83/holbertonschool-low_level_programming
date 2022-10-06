86e8561881643977d3260de516c0dd9f6736dcd2
MBK83 <rayanembarek07@gmail.com>
2022-10-06 02:07:09 -0700

    8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *  * jack_bauer - Entry point
+ *   *
+ *    * Return: 0
+ *     */
+
+void jack_bauer(void)
+{
+		int hora;
+			int min;
+
+				for (hora = 0; hora < 24; hora++)
+						{
+									for (min = 0; min < 60; min++)
+												{
+																_putchar((hora / 10) + '0');
+																			_putchar((hora % 10) + '0');
+																						_putchar(':');
+																									_putchar((min / 10) + '0');
+																												_putchar((min % 10) + '0');
+																															_putchar('\n');
+																																	}
+										}
+}
