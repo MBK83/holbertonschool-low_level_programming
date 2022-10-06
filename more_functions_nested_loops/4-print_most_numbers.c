7f218157e5f83c1ffaf718445da08c03f43cef0e
MBK83 <rayanembarek07@gmail.com>
2022-10-06 14:27:27 -0700

    t4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *  * print_most_numbers - Retry point
+ *   *
+ *    * Return: 0
+ **/
+
+void print_most_numbers(void)
+{
+		int a;
+
+		for (a = 48; a <= 57; a++)
+		{
+			if (a != 50 && a != 52)
+				_putchar(a);
+		}
+				_putchar('\n');
+}
