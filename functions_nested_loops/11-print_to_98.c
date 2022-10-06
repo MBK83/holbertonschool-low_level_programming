75338d7f302ff6b86e4ed8c7020cca43ea4667a3
MBK83 <rayanembarek07@gmail.com>
2022-10-06 04:43:19 -0700

    11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -7,38 +7,37 @@
  *    * @n: todos los numeros naturales
  *     *
  *      * Return: 0
- *       */
+ **/
 
 void print_to_98(int n)
 {
-		if (n < 98)
-				{
-							while (n <= 98)
-										{
-														printf("%d", n);
-																	if (n != 98)
-																					{
-																										printf(", ");
-																													}
-																				n++;
-																						}
-								}
-			else if (n > 98)
-					{
-								while (n >= 98)
-											{
-															printf("%d", n);
-																		if (n != 98)
-																						{
-																											printf(", ");
-																														}
-																					n--;
-																							}
-									}
-				else
-						{
-									printf("98");
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
+		}
+	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
+		}
+	}
+	else
+	{
+							printf("98");
 										}
-					printf("\n");
+				printf("\n");
 }
-														
