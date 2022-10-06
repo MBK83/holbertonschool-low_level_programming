65262152c418a9f96c1adb3e758b3c448cdad823
MBK83 <rayanembarek07@gmail.com>
2022-10-06 01:37:00 -0700

    3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *  * _islower - Entry point
+ *   * @c: int
+ *    *
+ *     * Return: 0
+ **/
+int _islower(int c)
+{
+
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+								}
+	return (0);
+
+}
