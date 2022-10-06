29c21d58d407df062a2e11876c163d2a4eb41373
MBK83 <rayanembarek07@gmail.com>
2022-10-06 15:07:01 -0700

    TACHE5 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *  * print_line - Retry point
+ *   *
+ *    * @n: function that draws a straight line in the terminal.
+ *     * Return: 0
+ *      */
+
+void print_line(int n)
+{
+	int a = 1;
+
+	while (a <= n)
+	{
+		_putchar(95);
+		a++;
+	}
+	_putchar('\n');
+}
