c5afeb3745ace33c645c8f3b15b72d88db687438
MBK83 <rayanembarek07@gmail.com>
2022-10-06 14:19:55 -0700

    t3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,18 @@
 #include "main.h"
+
 /**
- *  * print_numbers - Retry point
- *   *
- *    * Return: 0
+ *   * print_numbers - Print the numbers since 0 up to 9
+ *     *
+ *       * Return: The numbers since 0 up to 9
  **/
 void print_numbers(void)
 {
-	int a;
+	int x;
 
-	for (a = 48; a <= 57; a++)
+	for (x = 0; x <= 9; x++)
 	{
-		_putchar(a);
+		_putchar(x + '0');
 	}
+
 	_putchar('\n');
 }
