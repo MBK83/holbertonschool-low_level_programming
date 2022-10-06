61dd99f6db3e75821018f10430f1e531a48221de
MBK83 <rayanembarek07@gmail.com>
2022-10-06 14:33:17 -0700

    5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -6,19 +6,20 @@
  **/
 void more_numbers(void)
 {
-		int a;
-			int b;
+	int a;
+	int b;
 
-				for (a = 0; a <= 9; a++)
-						{
-									for (b = 0; b <= 14; b++)
-												{
-																if (b >= 10)
-																				{
-																									_putchar(b / 10 + '0');
-																												}
-																			_putchar(b % 10 + '0');
-																					}
-										_putchar('\n');
-											}
+	for (a = 0; a <= 9; a++)
+	{
+
+		for (b = 0; b <= 14; b++)
+		{
+			if (b >= 10)
+			{
+				_putchar(b / 10 + '0');
+			}
+			_putchar(b % 10 + '0');
+		}
+		_putchar('\n');
+	}
 }
