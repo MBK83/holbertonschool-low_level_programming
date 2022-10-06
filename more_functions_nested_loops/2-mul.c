05d94220746186a055183fc54bcc859a0f9f828f
MBK83 <rayanembarek07@gmail.com>
2022-10-06 15:59:52 -0700

    tache012

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+ /**
+  *  * mul - Retry point
+  *   *
+  *    * @a: integral1
+  *     * @b: integral2
+  *      *
+  *       * Return: 0
+  *        */
+
+ int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
