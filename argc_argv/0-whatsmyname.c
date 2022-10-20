c33a5a6f92d5d971b76a086420dbe155d2c60ab1
MBK83 <rayanembarek07@gmail.com>
2022-10-20 04:40:20 -0700

    t0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *  * main - It ain't what they call you, it's what you answer to
+ *   *
+ *    * @argc: If you rename the program, it will print the new name, without having
+ *     * @argv: You should not remove the path before the name of the program
+ *      *
+ *       * Return: 0 (Success)
+ **/
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+
+		printf("%s\n", argv[0]);
+
+	return (0);
+}
