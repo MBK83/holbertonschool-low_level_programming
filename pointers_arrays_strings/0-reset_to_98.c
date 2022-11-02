10fe874e3981266dd4e9675438e3f5aaee22c373
MBK83 <rayanembarek07@gmail.com>
2022-11-01 18:00:19 -0700

    tache

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,6 @@
-#include "main.h"
-/**
- *  * reset_to_98 - Retry point
- *   *
- *    * @n: Battery st.
- *     * Return: 0
- **/
-
-void reset_to_98(int *n)
+#include "main.h
+void reset_to_98(int *n);
 {
-		*n = 98;
+	*n = 98;
 
 }
