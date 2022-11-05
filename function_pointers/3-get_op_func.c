cabccebf131f5c3c48fd0a5d755be64cf89ca448
MBK83 <rayanembarek07@gmail.com>
2022-11-05 14:40:02 -0700

    t3

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -5,28 +5,28 @@
  *  * get_op_func - selects the correct func to perform the iperation
  *   * @s: operator passed as arguement to program
  *    * Return: a pointyer to the function
- *     */
+ **/
 
 int (*get_op_func(char *s))(int, int)
 {
-		op_t ops[] = {
-					{"+", op_add},
-							{"-", op_sub},
-									{"*", op_mul},
-											{"/", op_div},
-													{"%", op_mod},
-															{NULL, NULL}
-						};
-			int i;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
 
-				i = 0;
-					while (i < 5)
-							{
-										if (*s == *(ops[i]).op)
-													{
-																	return (*(ops[i]).f);
-																			}
-												i++;
-													}
-						return (NULL);
+	i = 0;
+	while (i < 5)
+	{
+		if (*s == *(ops[i]).op)
+		{
+			return (*(ops[i]).f);
+		}
+			i++;
+	}
+	return (NULL);
 }
