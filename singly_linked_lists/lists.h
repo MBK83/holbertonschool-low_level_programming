6298072d71a0048d5f075bbed8004e41a4778055
MBK83 <rayanembarek07@gmail.com>
2022-11-18 13:58:17 -0800

    t0

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,12 +1,18 @@
-#ifndef _LISTS_H_
-#define _LISTS_H_
-#include <stddef.h>
+#ifndef LISTS
+#define LISTS
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
 /**
  *  * struct list_s - singly linked list
- *   * @str: string
+ *   * @str: string - (malloc'ed string)
  *    * @len: length of the string
- *     * @next: point to the next node
- **/
+ *     * @next: points to the next node
+ *      *
+ *       * Description: singly linked list node structure
+ *        * for Holberton project
+ *         */
 typedef struct list_s
 {
 	char *str;
@@ -16,9 +22,7 @@ typedef struct list_s
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
-int _strlen(const char *s);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-void __attribute__ ((constructor)) premain();
 #endif
